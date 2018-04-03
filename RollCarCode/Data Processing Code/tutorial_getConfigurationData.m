288661c7b7dc4b181ef3f3f9c225073d8ef49e37
Colin Um <um_colin@berkeley.edu>
2018-04-02 23:28:55 -0700

    changed tutorial script

--- RollCarCode/Data Processing Code/tutorial_getConfigurationData.m
+++ RollCarCode/Data Processing Code/tutorial_getConfigurationData.m
@@ -1,7 +1,9 @@
 %% Usage of the getConfigurationData function.
 % This is a sample script demonstrating usage of the getConfigurationData
 % function.
-fileFolders={'Data/2x2x2_test_matrix/'};
+fileFolders={'/Users/UmColin/Documents/SHARED/6Sp18/6ME107/Roll Car/Github/ME107/RollCarCode/Data/2x2x2_test_matrix/'};
 configurations=getConfigurationData(fileFolders);
 % NOTE: getConfigurationData may take a long time to run.
-save configurations;
+trimmedConfigurations=trimRuns(configurations);
+combinedConfigurations=combineRuns(trimmedConfigurations);
+save combinedConfigurations;
