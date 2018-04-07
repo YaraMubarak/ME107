5f71f6f965d68a1af9a469582e92bd298304bff7
ptcarlson <ptcarlson@berkeley.edu>
2018-04-07 14:56:22 -0700

    Made very slight edits

--- RollCarCode/Data Processing Code/tutorial_getConfigurationData.m
+++ RollCarCode/Data Processing Code/tutorial_getConfigurationData.m
@@ -1,9 +1,9 @@
 %% Usage of the getConfigurationData function.
 % This is a sample script demonstrating usage of the getConfigurationData
 % function.
-fileFolders={'C:\Users\pcarl\Documents\College Things\ME 107\Roll Car\Github Code Repository\RollCarCode\Data\2x2x2_test_matrix'};
+fileFolders={'C:\Users\pcarl\Documents\College Things\ME 107\Roll Car\Github Code Repository\RollCarCode\Data\2x2x2_test_matrix\'};
 configurations_03_22 = getConfigurationData(fileFolders);
 % NOTE: getConfigurationData may take a long time to run.
-trimmedConfigurations_03_22=trimRuns(configurations_03_22);
-combinedConfigurations_03_22=combineRuns(trimmedConfigurations_03_22);
-save combinedConfigurations_03_22;
+trimmedConfigurations_03_22 = trimRuns(configurations_03_22);
+combinedConfigurations_03_22 = combineRuns(trimmedConfigurations_03_22);
+%save combinedConfigurations_03_22;
