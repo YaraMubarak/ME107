bb0887e9eac7c2e5af7fae9e750ca76a41abab6a
Colin Um <um_colin@berkeley.edu>
2018-04-09 22:36:34 -0700

    Generated graphs for displacement

--- RollCarCode/Data Processing Code/tutorial_getConfigurationData.m
+++ RollCarCode/Data Processing Code/tutorial_getConfigurationData.m
@@ -1,10 +1,14 @@
-%% Usage of the getConfigurationData function.
+%% Usage of the getConfigurationData function.  Loading Data.
 % This is a sample script demonstrating usage of the getConfigurationData
 % function.
-fileFolders={'C:\Users\pcarl\Documents\College Things\ME 107\Roll Car\Github Code Repository\RollCarCode\Data\2x2x2_test_matrix\'};
-configurations_03_22 = getConfigurationData(fileFolders);
+folder='C:\Users\pcarl\Documents\College Things\ME 107\Roll Car\Github Code Repository\RollCarCode\Data\2x2x2_test_matrix\';
+folder='/Users/UmColin/Documents/SHARED/6Sp18/6ME107/Roll Car/Github/ME107/RollCarCode/Data/';
+fileFolders={[folder '2x2x2_test_matrix/'],[folder '04-05-18/']};
+configurations_04_05 = getConfigurationData(fileFolders);
 % NOTE: getConfigurationData may take a long time to run.
-trimmedConfigurations_03_22 = trimRuns(configurations_03_22);
-combinedConfigurations_03_22 = combineRuns(trimmedConfigurations_03_22);
-averagedConfigurations_03_22 = averageRuns(combinedConfigurations_03_22);
-%save averagedConfigurations_03_22;
+%% Processing data.
+trim=false;
+trimmedConfigurations_04_05 = trimRuns(configurations_04_05,trim);
+combinedConfigurations_04_05 = combineRuns(trimmedConfigurations_04_05);
+averagedConfigurations_04_05 = averageRuns(combinedConfigurations_04_05);
+save(data_04_05);
