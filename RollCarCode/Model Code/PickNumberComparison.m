c047afd9a310da2f4205a0476178dbdda512c849
ptcarlson <ptcarlson@berkeley.edu>
2018-04-19 01:21:35 -0700

    more forgotten

--- RollCarCode/Model Code/PickNumberComparison.m
+++ RollCarCode/Model Code/PickNumberComparison.m
@@ -1,4 +1,4 @@
-%clear all; close all; clc;
+clear all; close all; clc;
 load configurations_04_12_untrimmed;
 for ConfigPick = 1:numel(averagedConfigurations_04_12)
     config = averagedConfigurations_04_12(ConfigPick);
